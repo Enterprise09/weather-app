41b9266ac9a778a891a593855cd58fca460ecd0c
Enterprise09 <goldenstaytes@gmail.com>
2021-06-24 15:44:38 +0900

    Get Location

--- App.js
+++ App.js
@@ -1,16 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
+import { Alert } from 'react-native';
 import React from 'react';
 import Loading from './Loading';
 import * as Location from "expo-location"
 
 export default class extends React.Component{
   getLocation = async()=> {
-    const location = await Location.getCurrentPositionAsync();
-    console.log(location);
-  }
-  
+    try {
+      await Location.requestBackgroundPermissionsAsync();
+      const location = await Location.getCurrentPositionAsync();
+      console.log(location);
+    } catch (error) {
+      Alert.alert("Can't find you.", "So Sad");
+    } 
+  };
+
   componentDidMount(){
-    this.location;
+    this.getLocation();
   }
 
   render(){
