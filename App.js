dde4043b0b4810b32ab060b8766519a091f99031
Enterprise09 <goldenstaytes@gmail.com>
2021-06-24 21:21:26 +0900

    Install Axios

--- App.js
+++ App.js
@@ -3,17 +3,24 @@ import { Alert } from 'react-native';
 import React from 'react';
 import Loading from './Loading';
 import * as Location from "expo-location"
+import axios from 'axios';
+
+const API_KEY = "422caf1114fed313af799fb184b2f130";
 
 export default class extends React.Component{
   state = {
     isLoading: true,
-  }
+  };
+  getWeather = async()=>{
+    const {data} = await axios.get()
+  };
   getLocation = async()=> {
     try {
       await Location.requestBackgroundPermissionsAsync();
       const {
         coords: {latitude, longitude}
       } = await Location.getCurrentPositionAsync();
+      this.getWeather(latitude, longitude);
       this.setState({isLoading: true});
     } catch (error) {
       Alert.alert("Can't find you.", "So Sad");
